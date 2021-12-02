4929e7e13e153f000752aef95196dbd2f98a6283
AyoubBF <ayoub.benfraj013@gmail.com>
2021-12-02 19:51:24 +0100

    commit1

--- defiprincipalnuitinfo2021/src/App.js
+++ defiprincipalnuitinfo2021/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer"
+import FormConnexion from "./components/Form/FormConnexion"
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <div className="content"></div>
+      <FormConnexion/>
+
       <Footer />
     </div>
   );
