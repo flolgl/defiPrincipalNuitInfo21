5f1a7691c48b040253e29d1176055c7dcda9bb56
Flo <florianlegal@outlook.fr>
2021-12-02 23:58:19 +0100

    Router

--- defiprincipalnuitinfo2021/src/App.js
+++ defiprincipalnuitinfo2021/src/App.js
@@ -5,16 +5,18 @@ import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer"
 import FormConnexion from "./components/Form/FormConnexion"
 import Button from '@mui/material/Button';
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <div className="duMargin">
 
-      </div>
-      <FormConnexion variant="contained">Hello World</FormConnexion>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<FormConnexion variant="contained"/>} />
+    
+      </Routes>
       <Footer/>
-
     </div>
   );
 }
