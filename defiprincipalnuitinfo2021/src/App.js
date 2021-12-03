8b632ce2606ca98546a50b06cd0fece8fa620046
AyoubBF <ayoub.benfraj013@gmail.com>
2021-12-03 02:55:55 +0100

    page d'accueil

--- defiprincipalnuitinfo2021/src/App.js
+++ defiprincipalnuitinfo2021/src/App.js
@@ -1,27 +1,34 @@
-import * as React from 'react';
-import ReactDOM from 'react-dom';
+import * as React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
-import Footer from "./components/Footer/Footer"
-import FormConnexion from "./components/Form/FormConnexion"
-import FormInscription from "./components/Form/FormInscription"
-import Button from '@mui/material/Button';
+import Footer from "./components/Footer/Footer";
+import FormConnexion from "./components/Form/FormConnexion";
+import FormInscription from "./components/Form/FormInscription";
 import { Routes, Route, Link } from "react-router-dom";
-import PageTPL from './components/PageTPL/PageTPL';
+import PageTPL from "./components/PageTPL/PageTPL";
+import BackgroundVideo from "./components/BackgroundVideo/BackgroundVideo";
 
 function App() {
   return (
     <div className="App">
-
-      <Navbar/>
+      <Navbar />
+      <BackgroundVideo />
       <Routes>
-        <Route path="/" element={<PageTPL variant="contained"/>} />
-        <Route path="/connexion" element={<FormConnexion variant="contained"/>} />
-        <Route path="/inscription" element={<FormInscription variant="contained"/>} />
-        <Route path="/ajouter" element={<FormConnexion variant="contained"/>} />
-    
+        <Route path="/" element={<PageTPL variant="contained" />} />
+        <Route
+          path="/connexion"
+          element={<FormConnexion variant="contained" />}
+        />
+        <Route
+          path="/inscription"
+          element={<FormInscription variant="contained" />}
+        />
+        <Route
+          path="/ajouter"
+          element={<FormConnexion variant="contained" />}
+        />
       </Routes>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
