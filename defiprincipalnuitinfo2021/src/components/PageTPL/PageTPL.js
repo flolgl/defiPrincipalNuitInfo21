71f98a9e95c507c5fc6382680b3cbf7e6931528d
AyoubBF <ayoub.benfraj013@gmail.com>
2021-12-03 05:31:53 +0100

    content

--- defiprincipalnuitinfo2021/src/components/PageTPL/PageTPL.js
+++ defiprincipalnuitinfo2021/src/components/PageTPL/PageTPL.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import ReactDom from "react-dom";
 import BackgroundVideo from "../BackgroundVideo/BackgroundVideo";
+import SupportIcon from "@mui/icons-material/Support";
 
 import "./PageTPL.css";
 
@@ -29,9 +30,39 @@ class PageTPL extends Component {
       <div>
         <BackgroundVideo />
         <div className="section1">
-            <div></div>
-            <div></div>
-            <div></div>
+          <div className="grid_section1">
+            <img
+              className="img_size"
+              src="https://cdn-icons-png.flaticon.com/512/342/342381.png"
+            />
+            <div className="section_text">
+              Ces sauveteurs, habitants du dunkerquois (de Bray-Dunes à
+              Grand-Fort-Philippe), ont participé à plus de 900 sauvetages en
+              mer et plus de 1100 sauvetages individuels.
+            </div>
+          </div>
+          <div className="grid_section1">
+            <img
+              className="img_size"
+              src="http://assets.stickpng.com/thumbs/5b06c233fad1cae04539afe3.png"
+            />
+            <div className="section_text">
+              "Les sauvetages? Mais tout le monde en fait … Si un homme tombe
+              dans la « Baille » est ce qu’on ne va pas le chercher? Il n’y a à
+              cela aucun mérite…" - Arsène Bossu
+            </div>
+          </div>
+          <div className="grid_section1">
+            <img
+              className="img_size"
+              src="https://images.vexels.com/media/users/3/130014/isolated/preview/3a93b4202dc70070d1d6a87f656bb267-danger-sign.png
+              "
+            />
+            <div className="section_text">
+              La présence de bancs de sable a toujours rendu la navigation
+              périlleuse au large du dunkerquois.
+            </div>
+          </div>
         </div>
       </div>
     );
