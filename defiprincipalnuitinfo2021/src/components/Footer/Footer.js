2ff965b074b198f4cd95778f0f225dc2caff66bf
Laurent Ngeth <l.ngeth@yahoo.fr>
2021-12-03 05:36:04 +0100

    Add files via upload
    footer

--- defiprincipalnuitinfo2021/src/components/Footer/Footer.js
+++ defiprincipalnuitinfo2021/src/components/Footer/Footer.js
@@ -8,70 +8,72 @@ import Grid from '@mui/material/Grid';
 function FormRow() {
     return (
         <React.Fragment>
-            <Grid item md={2} sm={6} xs={12}>
-                <Link href="https://sauveteurdudunkerquois.fr/qui-sommes-nous/" underline="hover" color={"white"}>
-                    {'Qui sommes-nous ?'}
+            <Grid item md={1} sm={6} xs={12}>
+                <Link className={'foot_item'} href="https://sauveteurdudunkerquois.fr/qui-sommes-nous/" underline="none" color={"white"}>
+                    {'Qui sommes-nous'}
                 </Link>
             </Grid>
-            <Grid item md={2} sm={6} xs={12}>
-                <Link href="https://sauveteurdudunkerquois.fr/presse/" underline="hover" color={"white"}>
+            <Grid item md={1} sm={6} xs={12}>
+                <Link className={'foot_item'} href="https://sauveteurdudunkerquois.fr/presse/" underline="none" color={"white"}>
                     {'Presse'}
                 </Link>
             </Grid>
-            <Grid item md={2} sm={6} xs={12}>
-                <Link href="https://sauveteurdudunkerquois.fr/sites-amis/" underline="hover" color={"white"}>
+            <Grid item md={1} sm={6} xs={12}>
+                <Link className={'foot_item'} href="https://sauveteurdudunkerquois.fr/sites-amis/" underline="none" color={"white"}>
                     {'Sites amis'}
                 </Link>
             </Grid>
-            <Grid item md={2} sm={6} xs={12}>
-                <Link href="https://sauveteurdudunkerquois.fr/plan-du-site/" underline="hover" color={"white"}>
+            <Grid item md={1} sm={6} xs={12}>
+                <Link className={'foot_item'} href="https://sauveteurdudunkerquois.fr/plan-du-site/" underline="none" color={"white"}>
                     {'Plan du site'}
                 </Link>
             </Grid>
-            <Grid item md={2} sm={6} xs={12}>
-                <Link href="https://sauveteurdudunkerquois.fr/lettre-hebdomadaire/" underline="hover" color={"white"}>
+            <Grid item md={1} sm={6} xs={12}>
+                <Link className={'foot_item'} href="https://sauveteurdudunkerquois.fr/lettre-hebdomadaire/" underline="none" color={"white"}>
                     {'Lettre hebdomadaire'}
                 </Link>
             </Grid>
-            <Grid item md={2} sm={6} xs={12}>
-                    <Link href="https://sauveteurdudunkerquois.fr/exprimez-vous/" underline="hover" color={"white"}>
-                        {'Exprimez-vous'}
-                    </Link>
+            <Grid item md={1} sm={6} xs={12}>
+                <Link className={'foot_item'} href="https://sauveteurdudunkerquois.fr/exprimez-vous/" underline="none" color={"white"}>
+                    {'Exprimez-vous'}
+                </Link>
             </Grid>
         </React.Fragment>
     );
 }
 
-const Footer = () => {
-  return (
-    <div className="footer">
-      <footer class="footer-distributed">
-        <div class="footer-left">
+const Footer = (props) => {
+    return (
+        <footer class="footer-distributed">
             <h3>Ma Libellule</h3>
-        </div>
+            <div className="container">
+                <span style={{ color: props.stateDark ? "grey" : "yellow" }}>☀︎</span>
+                <div className="switch-checkbox">
+                    <label className="switch">
+                        <input type="checkbox" onChange={() => props.setStateDark(!props.stateDark)} />
+                        <span className="slider round"> </span>
+                    </label>
+                </div>
+                <span style={{ color: props.stateDark ? "#c96dfd" : "grey" }}>☽</span>
+            </div>
             <Box
                 sx={{
                     mx: 'auto',
                     bgcolor: 'primary.main',
                     color: 'blue',
                     width: 200,
-                    p: 1,
-                    m: 1,
                     borderRadius: 1,
                     textAlign: 'center',
                     display: 'inline',
-                    alignContent: 'space-around'
+                    marginTop: '10px'
                 }}
             >
-                <Grid container spacing={1}>
-                    <Grid container item spacing={3}>
-                        <FormRow />
-                    </Grid>
+                <Grid container item spacing={5} sx={{justifyContent:'space-around', marginTop: 0}}>
+                    <FormRow/>
                 </Grid>
             </Box>
-      </footer>
-    </div>
-  );
+        </footer>
+    );
 };
 
 export default Footer;
